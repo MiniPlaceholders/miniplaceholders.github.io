4abfb649690bd3a12579397b9afa749b8d445d88
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-08-23 20:51:50 -0500

    Added test deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MiniPlaceholders', // Usually your GitHub org/user name.
   projectName: 'MiniPlaceholders', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
